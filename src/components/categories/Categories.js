b830197caf9fed9ad448025150ff6b6dd77e2d0b
v-nesh <vignesh.leet@gmail.com>
2023-01-13 23:17:41 +0530

    initial commit

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Categories.css";
+
+const capitalize = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="--flex-center">
+      {categories.map((category, index) => {
+        return (
+          <button
+            key={index}
+            type="button"
+            className="btn --btn --btn-secondary"
+            onClick={() => filterItems(category)}
+          >
+            {capitalize(category)}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/components/product/ProductList.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import Categories from "../categories/Categories";
+import Search from "../search/Search";
+import Product from "../product/Product";
+import "./ProductList.css";
+import { products as productData } from "../../products-data";
+
+const allCategories = [
+  "all",
+  ...new Set(productData.map((product) => product.category)),
+];
+
+const ProductList = () => {
+  const [products, setProducts] = useState(productData);
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filterProducts = (category) => {
+    if (category === "all") {
+      setProducts(productData);
+      return;
+    }
+    const newProduct = productData.filter(
+      (product) => product.category === category
+    );
+    setProducts(newProduct);
+  };
+
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((product) =>
+        product.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, products]);
+  return (
+    <>
+      <div className="header">
+        <header className="container">
+          <h1 className="--color-white --text-center">
+            <span className="--color-danger">Product</span> filter
+          </h1>
+          <div className="--flex-between --flex-dir-column --py">
+            <Search inputValue={search} onInputChange={handleSearch} />
+            <Categories categories={categories} filterItems={filterProducts} />
+          </div>
+        </header>
+      </div>
+      <div className="product-container">
+        <div className="products container --grid-25 --py2">
+          {filteredProducts.length === 0 ? (
+            <h3>No product found</h3>
+          ) : (
+            filteredProducts.map((product) => {
+              const { id, title, img, price } = product;
+              return (
+                <div key={id}>
+                  <Product title={title} img={img} price={price} />
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
